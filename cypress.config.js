fbf9d3afb63da8cddc7c31a76d83ceeb5232741b
Bruno Cassiano <bruno15l@hotmail.com>
2024-03-24 16:41:30 -0300

    setup 9

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,12 @@ module.exports = defineConfig({
   e2e: {
     supportFile: "./node_modules/test-acceptance-cypress/cypress/support/e2e.js",
     specPattern: "features/execution/*.feature",
+    retries: 0,
+    responseTimeout: 30000,
+    experimentalInteractiveRunEvents: true,
+    screenshotOnRunFailure: true,
+    video: false,
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
