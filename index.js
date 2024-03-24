645c2d83db75142136901f352fdf249ae4211a1b
Bruno Cassiano <bruno15l@hotmail.com>
2024-03-24 19:40:19 -0300

    setup 20

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ console.log("Biblioteca principal carregada");
 
 const runTests = require('./src/cypress/commands/runTest');
 const openCypress = require('./src/cypress/commands/openCypress');
-const setResponseBody = require('./src/cypress/rest/rest')
-// import restFull from './src/cypress/rest/rest';
+const restFull = require('./src/cypress/rest/rest')
 
-module.exports = { openCypress, runTests, setResponseBody };
+module.exports = { openCypress, runTests, restFull };
--- src/cypress/rest/rest.js
+++ src/cypress/rest/rest.js
@@ -6,52 +6,56 @@ let responseBody;
 
 // function readTable(data = {}) {
 //     return object(rest(data.rawTable || []));
-// };s
-
-// export default class restFull {
-
-//     static setResponseBody = _responseBody => {
-//         responseBody = _responseBody;
-//     };
-
-//     static getResponseBody = _ => responseBody;
-
-//     static setResponseStatusCode = _responseStatusCode => {
-//         responseStatusCode = _responseStatusCode;
-//     };
-
-//     static getResponseStatusCode = _ => responseStatusCode;
-
-//     /**
-//      * STANDARD REST CALL, MUST BE PASSED THE VERB, URI AND PATH
-//      * @param {*} alias - Name to request
-//      * @param {*} reqType - Method (Ex: POST, GET, PUT or DELETE) verb
-//      * @param {*} uri - URI (BaseUrl)
-//      * @param {*} path - PATH (EndPoint)
-//      * @param {*} log  - LOG (true or false), default true
-//      */
-//     static requestRestFull(alias, reqType, uri, path, log = true){
-
-//         cy.api({
-//             method: reqType,
-//             url: uri + path,
-//             failOnStatusCode: false,
-//             log: log
-//         }).as(alias).then((resp) => {
-//             this.setResponseStatusCode(resp.status);
-//             this.setResponseBody(resp.body);
-
-//             //SET LOG
-//             logRest.setRequet(uri, path, reqType, resp.status, resp.body);
-//         })
-
-//     };
 // };
 
-// export default restFull;
+export default class restFull {
 
-    function setResponseBody (_responseBody){
+    constructor(){
+
+    }
+
+    static setResponseBody = _responseBody => {
         responseBody = _responseBody;
     };
 
-    module.exports = setResponseBody;
+    static getResponseBody = _ => responseBody;
+
+    static setResponseStatusCode = _responseStatusCode => {
+        responseStatusCode = _responseStatusCode;
+    };
+
+    static getResponseStatusCode = _ => responseStatusCode;
+
+    /**
+     * STANDARD REST CALL, MUST BE PASSED THE VERB, URI AND PATH
+     * @param {*} alias - Name to request
+     * @param {*} reqType - Method (Ex: POST, GET, PUT or DELETE) verb
+     * @param {*} uri - URI (BaseUrl)
+     * @param {*} path - PATH (EndPoint)
+     * @param {*} log  - LOG (true or false), default true
+     */
+    static requestRestFull(alias, reqType, uri, path, log = true){
+
+        cy.api({
+            method: reqType,
+            url: uri + path,
+            failOnStatusCode: false,
+            log: log
+        }).as(alias).then((resp) => {
+            this.setResponseStatusCode(resp.status);
+            this.setResponseBody(resp.body);
+
+            //SET LOG
+            logRest.setRequet(uri, path, reqType, resp.status, resp.body);
+        })
+
+    };
+};
+
+// export default restFull;
+
+    // function setResponseBody (_responseBody){
+    //     responseBody = _responseBody;
+    // };
+
+    // module.exports = setResponseBody;
