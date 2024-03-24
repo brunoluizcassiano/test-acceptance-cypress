25af0c57a03bd1c5cb18bc4681df093c63193bb9
Bruno Cassiano <bruno15l@hotmail.com>
2024-03-24 16:23:40 -0300

    setup 8

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    supportFile: "cypress/support/e2e.js",
     specPattern: "features/execution/*.feature",
     setupNodeEvents(on, config) {
       // implement node event listeners here
