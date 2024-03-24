bae3f70fa673c15f4f90c30a511b766e9e0a038f
Bruno Cassiano <bruno15l@hotmail.com>
2024-03-24 17:13:49 -0300

    setup 12

--- src/cypress/commands/openCypress.js
+++ src/cypress/commands/openCypress.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 function openCypress(){
     const options = {
-        configFile: path.resolve(__dirname, 'cypress.config.js')
+        configFile: path.resolve(__dirname, '../../../cypress.config.js')
     };
     
     return cypress.open(options).then((results) => {
--- src/cypress/commands/runTest.js
+++ src/cypress/commands/runTest.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 function runTests(){
     const options = {
-        configFile: path.resolve(__dirname, 'cypress.config.js')
+        configFile: path.resolve(__dirname, '../../../cypress.config.js')
     };
 
     return cypress.run(options).then((results) => {
