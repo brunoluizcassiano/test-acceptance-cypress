c264d29dd64ef972a99d6c7476590074493fe739
Bruno Cassiano <bruno15l@hotmail.com>
2024-03-25 07:31:41 -0300

    setup 32

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ console.log("Biblioteca principal carregada");
 
 const runTests = require('./src/cypress/commands/runTest');
 const openCypress = require('./src/cypress/commands/openCypress');
-const restFull = require('./src/cypress/rest/rest');
+// const restFull = require('./src/cypress/rest/rest');
 
-module.exports = { openCypress, runTests, restFull };
+module.exports = { openCypress, runTests };
 
 // module.exports = { restFull };
